40c30afb1bff1bf1c9729fdcf9a6b0585a0c83d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-30 15:10:51 +0000

    Deploy amitsingh-007/uglify-js-compiler to github.com/amitsingh-007/uglify-js-compiler.git:gh-pages

---
+++ precache-manifest.996d7610fa7cba74859807fd8a175327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2155d1961204e122921c34d488033bc",
+    "url": "/uglify-js-compiler/index.html"
+  },
+  {
+    "revision": "142bcec4476218c2729b",
+    "url": "/uglify-js-compiler/static/js/2.e6bcf5ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/uglify-js-compiler/static/js/2.e6bcf5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4226d69839471ac7b1c0",
+    "url": "/uglify-js-compiler/static/js/main.a9d78c4b.chunk.js"
+  },
+  {
+    "revision": "fd711cbd501ddd47bdf7",
+    "url": "/uglify-js-compiler/static/js/runtime-main.550eee7b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uglify-js-compiler/precache-manifest.b73219155aa6c6ad253cb5b416dd44c6.js"
+  "/uglify-js-compiler/precache-manifest.996d7610fa7cba74859807fd8a175327.js"
 );
 
 self.addEventListener('message', (event) => {
