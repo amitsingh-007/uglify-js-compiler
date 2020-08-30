6b9a8f46809362cf4ac93273b3a100bdd57b4c13
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-30 09:06:17 +0000

    Deploy amitsingh-007/uglify-js-compiler to github.com/amitsingh-007/uglify-js-compiler.git:gh-pages

---
+++ static/js/main.6837655e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpuglify-js-compiler"]=this["webpackJsonpuglify-js-compiler"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),c=n(2);var l=function(){return a.a.createElement("div",{className:"App"})};n.n(c).a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(l,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.6837655e.chunk.js.map
