07226090ea8bb6572578dd5bb9c1e7c9ea3f0038
Amit Singh <amitsingh5198@gmail.com>
2024-07-09 20:52:30 +0530

    Fix vercel build

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'; // Not working
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
+  server: {
+    preset: 'vercel',
+    prerender: {
+      crawlLinks: true,
+    },
+  },
   vite: {
     plugins: [
       tsconfigPaths(),
