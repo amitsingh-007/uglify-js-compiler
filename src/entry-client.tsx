d158e111eaab7e7f00c7b661c20997281ad8b339
Amit Singh <amitsingh5198@gmail.com>
2024-07-11 19:58:47 +0530

    Update deps

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,5 +1,7 @@
 // @refresh reload
 import { mount, StartClient } from '@solidjs/start/client';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-mount(() => <StartClient />, document.getElementById('app')!);
+const appNode = document.getElementById('app');
+if (appNode) {
+  mount(() => <StartClient />, appNode);
+}
